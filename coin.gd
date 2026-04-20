64857114b3d016e40694ed6fe639145d50abfc2d
Jonathan Lau <jonlau228@hotmail.com>
2026-04-19 18:00:27 -0700

    LauJonathan_FinalProject

--- coin.gd
+++ coin.gd
@@ -1,13 +1,16 @@
 extends Area3D
 
+@onready var sound = $AudioStreamPlayer3D
 @onready var mesh = $MeshInstance3D
 @onready var collision = $CollisionShape3D
 
 func _on_body_entered(body: Node3D) -> void:
 	if body.has_method("on_coin_collected"):
 		body.on_coin_collected()
+		sound.play()
 		mesh.visible = false
 		collision.set_deferred("disabled", true)
+		await sound.finished
 
 func respawn():
 	mesh.visible = true
